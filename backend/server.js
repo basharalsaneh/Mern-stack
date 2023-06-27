7dc9caed2a52ab4e17d432408477298051579328
Bashar Alsaneh <83920082+basharalsaneh@users.noreply.github.com>
2023-06-27 13:14:14 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000/",
-      "https://task-manager-apzo.onrender.com",
+      "https://task-manager-apzo-z2c9.onrender.com",
     ],
   })
 );
